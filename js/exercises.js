ddfc00e8a49cca88386a2c958a470ec77e3e249f
Lucia Barraza <luchyinthesky77@gmail.com>
2023-10-18 23:26:16 -0300

    ejercicio 2  listo

--- js/exercises.js
+++ js/exercises.js
@@ -1,6 +1,12 @@
 /*
-Ejercicio
-1
+Ejercicio 1: Variables y Operaciones Matemáticas
+
+Crea dos variables, numero1 y numero2, y asígnales valores numéricos. Luego, realiza las siguientes operaciones y muestra los resultados en la consola:
+
+Suma de numero1 y numero2.
+Resta de numero1 y numero2.
+Multiplicación de numero1 y numero2.
+División de numero1 entre numero2.
 */
 
 // definir variables
@@ -31,3 +37,16 @@ console.log("División")
 console.log(numero1/numero2);
 console.log("División de numero1 y numero2: " + (numero1 / numero2));
 console.log("División de " + numero1 + " y " + numero2 + ": " + (numero1 / numero2));
+
+/*
+Ejercicio 2: Concatenación de Cadenas
+Crea dos variables, nombre y apellido, y asígnales valores de tu elección. Luego, crea una tercera variable llamada nombreCompleto que contenga la concatenación de nombre y apellido. Muestra nombreCompleto en la consola.
+*/
+
+// definir variables
+let nombre = "Janis";
+let apellido = "Joplin";
+
+let nombreCompleto = nombre + " " + apellido;
+
+console.log(nombreCompleto);
