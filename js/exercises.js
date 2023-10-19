19eda2c5dd7c3d2bf691c47506e95d6910ceb5b7
Lucia Barraza <luchyinthesky77@gmail.com>
2023-10-18 23:06:44 -0300

    ejercicio 1 listo

---
+++ js/exercises.js
@@ -0,0 +1,33 @@
+/*
+Ejercicio
+1
+*/
+
+// definir variables
+let numero1 = 23;
+let numero2 = 17;
+
+//suma
+console.log("Suma")
+console.log(numero1+numero2)
+console.log("Suma de numero1 y numero2: " + (numero1 + numero2));
+console.log("Suma de " + numero1 + " y " + numero2 + ": " + (numero1 + numero2));
+
+//resta
+console.log("Resta")
+console.log(numero1-numero2);
+console.log("Resta de numero1 y numero2: " + (numero1 - numero2));
+console.log("Resta de " + numero1 + " y " + numero2 + ": " + (numero1 - numero2));
+
+//multiplicación
+console.log("Multiplicación")
+console.log(numero1*numero2);
+console.log("Multiplicación de numero1 y numero2: " + (numero1 * numero2));
+console.log("Multiplicación de " + numero1 + " y " + numero2 + ": " + (numero1 * numero2));
+
+
+//división
+console.log("División")
+console.log(numero1/numero2);
+console.log("División de numero1 y numero2: " + (numero1 / numero2));
+console.log("División de " + numero1 + " y " + numero2 + ": " + (numero1 / numero2));
