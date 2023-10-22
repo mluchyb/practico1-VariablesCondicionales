777e58f8430e282f7ff058e9aaf7c0d852437e42
Lucia Barraza <luchyinthesky77@gmail.com>
2023-10-21 21:15:14 -0300

    se crearon paginas nuevas para los ejercicios del Trabajo practico

---
+++ js/TP-ex4.js
@@ -0,0 +1,11 @@
+/*
+4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+Ejemplo: 
+input: Coder         Output: Hola Coder
+*/
+
+let nombreUsuario = prompt('Ingrese su nombre');
+
+let color = prompt("Ingrese su color favorito");
+
+console.log('Hola '+ nombreUsuario);
---
+++ js/TP-ex8.js
@@ -0,0 +1,7 @@
+/*
+8.- Escribe un programa que pida un número y diga si es divisible por 2
+Ejemplo: 
+
+input: 10              Output: El 10 es divisible por 2.
+input: 15              Output: El 15 no es divisible por 2.
+*/
